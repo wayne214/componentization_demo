762e5d098193face3a6e32225c01e8cac97f0e49
wangjunwei <wangjunwei2357@foryou56.com>
2021-09-14 15:54:00 +0800

    组件化

--- login/src/main/java/com/example/login/MainActivity.java
+++ login/src/main/java/com/example/login/MainActivity.java
@@ -3,15 +3,43 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 
 @Route(path = "/account/login")
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        updateLoginState();
+    }
+
+    private void initView() {
+        tvState = (TextView) findViewById(R.id.tv_login_state);
+    }
+
+    public void login(View view) {
+        AccountUtils.userInfo = new UserInfo("10086", "Admin");
+        updateLoginState();
+    }
+
+    public void exit(View view) {
+        AccountUtils.userInfo = null;
+        updateLoginState();
+    }
+
+    public void loginShare(View view) {
+        ARouter.getInstance().build("/share/share").withString("share_content", "分享数据到微博").navigation();
+    }
+
+    private void updateLoginState() {
+        tvState.setText("这里是登录界面：" + (AccountUtils.userInfo == null ? "未登录" : AccountUtils.userInfo.getUserName()));
     }
 }
--- share/src/main/java/com/example/share/MainActivity.java
+++ share/src/main/java/com/example/share/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.share;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.example.componentbase.ServiceFactory;
+
+@Route(path = "/share/share")
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getIntent() != null) {
+            String content = getIntent().getStringExtra("share_content");
+            if (!TextUtils.isEmpty(content)) {
+                ((TextView) findViewById(R.id.share_content)).setText(content);
+            }
+        }
+    }
+
+    private void share() {
+        if (ServiceFactory.getInstance().getAccountServices().isLogin()) {
+            Toast.makeText(this, "分享成功", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "分享失败：用户未登录", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void shareLogin(View view) {
+        ARouter.getInstance().build("/account/login").navigation();
     }
 }
